ce1beade1151e2f1da2b2c65112699619794ca74
isisnaldojoao <isisnaldojoao@gmail.com>
2023-10-06 15:49:20 -0300

    version final

---
+++ index.js
@@ -0,0 +1,40 @@
+let qtdWin  = 80;
+let qtdLose = 15;
+
+
+function calcRanked(){
+    let saldoVitorias = qtdWin - qtdLose;
+
+    switch(true){
+        case saldoVitorias < 10:
+        var nivel = "Ferro"
+        break;
+
+        case saldoVitorias >11 && saldoVitorias<20:
+        var nivel = "Bronze"
+        break;
+
+        case saldoVitorias >21 && saldoVitorias<50:
+        var nivel = "Prata"
+        break;
+
+        case saldoVitorias >51 && saldoVitorias<80:
+        var nivel = "Ouro"
+        break;
+
+        case saldoVitorias >81 && saldoVitorias<90:
+        var nivel = "Diamente"
+        break;
+
+        case saldoVitorias >91 && saldoVitorias<100:
+        var nivel = "Lendário"
+        break;
+
+        case saldoVitorias> 101:
+        var nivel = "Imortal"
+        break;
+    }
+    console.log("O Héroi tem saldo de "+saldoVitorias+" Vitórias e está no nivel de "+nivel)
+}
+
+calcRanked()
